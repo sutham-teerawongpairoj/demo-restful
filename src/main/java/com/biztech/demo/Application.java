05a59586933a39ffefe44164b3e28ac4ded317c9
LENOVO <sutham.teerawongpairoj@gmail.com>
2018-07-18 11:02:12 +0700

    Test config used external profile

--- src/main/java/com/biztech/demo/Application.java
+++ src/main/java/com/biztech/demo/Application.java
@@ -1,5 +1,6 @@
 package com.biztech.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,33 +11,33 @@ import java.util.Properties;
 public class Application extends SpringBootServletInitializer {
 //public class Application {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 
 //    @Override
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 //        return builder.sources(Application.class);
 //    }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
-        return springApplicationBuilder
-                .sources(Application.class)
-                .properties(getProperties());
-    }
-
-    public static void main(String[] args) {
-
-        SpringApplicationBuilder springApplicationBuilder = (SpringApplicationBuilder) new SpringApplicationBuilder(Application.class)
-                .sources(Application.class)
-                .properties(getProperties())
-                .run(args);
-    }
-
-    static Properties getProperties() {
-        Properties props = new Properties();
-        props.put("spring.config.location", "classpath:demo-restful/");
-        return props;
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+//        return springApplicationBuilder
+//                .sources(Application.class)
+//                .properties(getProperties());
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        SpringApplicationBuilder springApplicationBuilder = (SpringApplicationBuilder) new SpringApplicationBuilder(Application.class)
+//                .sources(Application.class)
+//                .properties(getProperties())
+//                .run(args);
+//    }
+//
+//    static Properties getProperties() {
+//        Properties props = new Properties();
+//        props.put("spring.config.location", "classpath:demo-restful/");
+//        return props;
+//    }
 }
